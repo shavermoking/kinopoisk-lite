6092682fe548b8acd812e98c15579f988bea9dfc
Никита Чуркин <churkin.n@matriks.group>
2023-12-13 16:50:23 +0300

    feat: added first routes

--- index.php
+++ index.php
@@ -2,4 +2,10 @@
 
 require_once __DIR__ . '/vendor/autoload.php';
 
+$routes = require_once __DIR__ . '/config/routes.php';
+
+$uri = $_SERVER['REQUEST_URI'];
+
+$routes[$uri]();
+
 echo 'Теперь все запросы тут';
