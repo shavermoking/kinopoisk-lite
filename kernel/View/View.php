957cd95c898db0e64e2cc3095a8f83ac9debdff1
Никита Чуркин <churkin.n@matriks.group>
2023-12-31 15:45:56 +0300

    feat: finished work on components and added new Exception

--- kernel/View/View.php
+++ kernel/View/View.php
@@ -2,15 +2,36 @@
 
 namespace App\Kernel\View;
 
+use App\Kernel\Exceptions\ViewNotFoundException;
+
 class View
 {
     public function page(string $name): void
     {
-        include_once APP_PATH . "/views/pages/$name.php";
+
+        $viewPath = APP_PATH."/views/pages/$name.php";
+
+        if (! file_exists($viewPath)){
+            throw new ViewNotFoundException("View $name not found");
+        }
+
+        extract([
+            'view' => $this
+        ]);
+
+        include_once $viewPath;
     }
 
     public function component(string $name): void
     {
-        include_once APP_PATH . "/views/components/$name.php";
+
+        $componentPath = APP_PATH . "/views/components/$name.php";
+
+        if (! file_exists($componentPath)){
+            echo "Component $name not found";
+            return;
+        }
+
+        include_once $componentPath;
     }
 }
--- views/pages/home.php
+++ views/pages/home.php
@@ -1,3 +1,10 @@
-<?php include __DIR__ . '/../components/start.php' ?>
+<?php
+/**
+ * @var \App\Kernel\View\View $view
+ */
+?>
+
+
+<?php $view->component('start') ?>
     <h1>Это домашняя страница</h1>
-<?php include __DIR__ . '/../components/end.php' ?>
+<?php $view->component('end');?>
